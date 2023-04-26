4c24bf6c88478cb60dae62fd3b056f9e11b3db51
Shivali Shrivastava <shivalishriv@umass.edu>
2023-04-26 11:14:09 -0400

    Added genre select functionality.

--- client/js/main.js
+++ client/js/main.js
@@ -5,6 +5,8 @@ const fourstar = document.getElementById("4star");
 const fivestar = document.getElementById("5star");
 const submit = document.getElementById("submit");
 
+const genres = document.getElementById("genres");
+
 const ratings = [onestar, twostar, threestar, fourstar, fivestar];
 
 const selectRating = (event) => {
@@ -42,17 +44,29 @@ const submitRating = () => {
     alert("submitted rating");
 }
 
-const initializehover = () => {
+const selectGenre = (event) => {
+    document.getElementById("selectedgenre").innerHTML = event.target.innerHTML;
+}
+
+const initializeRatingSelection = () => {
 
     for (let i = 0; i < ratings.length; i++) {
-
         ratings[i].addEventListener("mouseover", (event) => event.target.style.opacity = 0.7);
         ratings[i].addEventListener("mouseout", (event) => event.target.style.opacity = 1);
         ratings[i].addEventListener("click", selectRating);
-
     }
 
 }
 
-initializehover();
+const initializeGenreSelection = () => {
+
+    var genre = document.getElementById('genres').getElementsByClassName('genre');
+
+    for (let i = 0; i < genre.length; i++) {
+        genre[i].addEventListener("click", selectGenre);
+    }
+}
+
+initializeRatingSelection();
+initializeGenreSelection();
 submit.addEventListener("click", submitRating);
