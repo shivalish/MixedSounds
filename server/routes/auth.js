2b7dbc45f5c57d7e1898dd46f2512d1f9e773b96
Griffin Ridosh <36777380+griffinridosh@users.noreply.github.com>
2023-05-09 15:10:35 -0400

    .

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,21 +1,43 @@
 import { Router } from 'express';
-import passport from passport
-import expressSession from 'express-session'
+import { check, validationResult } from 'express-validator';
 import * as passport from 'passport'; 
-import { Strategy } from 'passport-local';
-import {dbclient} from "../db/db.js";
-import db from '../db/dbclient';
+import { Strategy as LocalStrategy } from 'passport-local';
+import * as dbclient from "../db/dbclient.js";
+
+
+// import db from '../db/dbclient';
 const authRouter = Router();
 
-authRouter.post('/login', passport.authenticate('local'), (req, res) => {
-    res.send(200);
-});
+// authRouter.post('/login', passport.authenticate('local'), (req, res) => {
+//     res.send(200);
+// });
+authRouter.post('/register' [
+    check(req.body.username)
+    .notEmpty()
+    .withMessage("username cannot be empty")
+    .isLength({min: 5})
+    .withMessage("username must be at least 5 characters long"),
+    check(req.body.password)
+    .notEmpty()
+    .withMessage("password cannot be empty")
+    .isLength({min: 5})
+    .withMessage("password must be at least 5 characters long")
+], async (req, res) => { 
+    if(!validationResult(req).isEmpty()){
+        console.log("error in validation");
+        return res.status(400);
+    }
+}
+)
 
-passport.use(new Strategy(
+passport.use(new LocalStrategy(
     async (username, password, done) => { 
-        const userDB = await client.db('musicdb')
+        const userDB = await dbclient.db('musicdb')
         const userCollection = await userDB.userCollection("userbase");
         const result = await userCollection.insertOne({username: username,  password: password});
         console.log("user is registerd");
     }
     ));
+
+
+    export default authRouter;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import home from "./routes/home.js";
 import history from "./routes/history.js";
 import playlists from "./routes/playlists.js";
-
+import authRoute from "./routes/auth.js";
 import express from "express";
 import path from "path";
 import { fileURLToPath } from 'url';
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use("/home", home);
 app.use("/history", history);
 app.use("/playlists", playlists);
+app.use("/auth", authRoute);
 
 
 app.listen(port, () => {
