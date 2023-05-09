cbce02cb02348687256f923717d3e33b1e021a7a
Griffin Ridosh <36777380+griffinridosh@users.noreply.github.com>
2023-05-09 14:44:05 -0400

    local passport strategy

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,21 @@
+import { Router } from 'express';
 import passport from passport
 import expressSession from 'express-session'
-import * as passport from 'passport';
+import * as passport from 'passport'; 
+import { Strategy } from 'passport-local';
+import {dbclient} from "../db/db.js";
+import db from '../db/dbclient';
+const authRouter = Router();
 
-app.use(expressSession(sessionConfig)); // session middleware 
+authRouter.post('/login', passport.authenticate('local'), (req, res) => {
+    res.send(200);
+});
 
+passport.use(new Strategy(
+    async (username, password, done) => { 
+        const userDB = await client.db('musicdb')
+        const userCollection = await userDB.userCollection("userbase");
+        const result = await userCollection.insertOne({username: username,  password: password});
+        console.log("user is registerd");
+    }
+    ));
